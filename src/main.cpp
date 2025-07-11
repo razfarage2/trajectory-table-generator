a046ed9976d7bd9b907a37b0aadc5ea65c39e0b8
razfa <razfarage3232@gmail.com>
2025-07-11 11:08:07 +0300

    initial-commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the iostream library for input/output operations
+
+int main() {
+    // This is the entry point of your C++ program.
+    // Code execution begins here.
+
+    std::cout << "Hello, C++ Project!" << std::endl; // Print a message to the console
+
+    return 0; // Indicate that the program executed successfully
+}
